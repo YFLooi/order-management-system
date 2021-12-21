d5675b5f8c2ed6851047514f87a559c6a494519c
YFLooi <yihfoo@gmail.com>
2021-12-21 19:56:41 +0800

    Set up rxjs pub sub demo

--- operations-portal/src/components/greeting-service/greeting-service.ts
+++ operations-portal/src/components/greeting-service/greeting-service.ts
@@ -1,5 +1,6 @@
 import { Subject } from "rxjs";
 
+// Ref: https://www.syntaxsuccess.com/viewarticle/pub-sub-with-rxjs-in-react
 // Declaring the observable like this allows it to be imported
 // wherever it should be subscribed to
 export default class GreetingService {
